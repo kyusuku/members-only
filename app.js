6e482d6d387d782d1d7992ea0f7f42aa13e75a5d
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-05-03 20:41:34 +0700

    Add locals.user setup back

--- app.js
+++ app.js
@@ -58,6 +58,11 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/", (req, res) => res.render("index"));
 
 app.post("/sign-up", async (req, res) => {
